e29e96360fb31e07c86ba912ffc3777a62ac1fa2
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-01-16 17:46:28 -0300

    Create __init__.py

---
+++ edite/__init__.py
@@ -0,0 +1,18 @@
+##criação do app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "4b6dceed7214c6316c75b11878760285"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "paginaInicial"
+
+from edite import rotas
